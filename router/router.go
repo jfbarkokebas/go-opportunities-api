f778c15404481600b7cfb5b660dfbf9a63d78b78
jfbarkokebas <jfbarkokebas@gmail.com>
2026-05-11 22:51:51 -0300

    implemented routes

--- router/router.go
+++ router/router.go
@@ -6,11 +6,7 @@ func Initialize() {
 	//Inicializa o Router utilizando as onfigurações default do gin
 	router := gin.Default()
 	//define uma rota
-	router.GET("ping", func(c *gin.Context) {
-		c.JSON(200, gin.H{
-			"message": "pong",
-		})
-	})
+	initializeRoutes(router)
 
 	router.Run(":8080") //list and serve  on 0.0.0.0:8080
 }
